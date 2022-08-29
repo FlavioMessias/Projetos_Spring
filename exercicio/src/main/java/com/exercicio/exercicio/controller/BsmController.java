ee1e307c844ba82c4235586ef0b794b3fda833f7
unknown <ankiflavio@gmail.com>
2022-08-29 17:06:26 -0300

    Realização do exercicio 1 Generation

---
+++ exercicio/src/main/java/com/exercicio/exercicio/controller/BsmController.java
@@ -0,0 +1,17 @@
+package com.exercicio.exercicio.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/bsm")
+public class BsmController {
+	
+	@GetMapping
+	public String bsm() {
+		return "MENTALIDADES \r\nOrientação ao futuro \r\nResponsabilidade pessoal \r\nMentalidade de crescimento \r\nPersistência\n \nHABILIDADES \r\nTrabalho em equipe \r\nOrientação ao detalhe \r\nPró-atividade \r\nComunicação";
+		
+	}
+
+}
