fcfddf51dc0f71dd27dffeb829a1c9012e898b06
unknown <ankiflavio@gmail.com>
2022-09-13 15:11:01 -0300

    security e service completos

--- games/src/main/java/com/generation/games/model/Produto.java
+++ games/src/main/java/com/generation/games/model/Produto.java
@@ -43,6 +43,10 @@ public class Produto {
 	@ManyToOne
 	@JsonIgnoreProperties("produto")
 	private Categoria categoria;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
 
 	public Long getId() {
 		return id;
@@ -98,6 +102,18 @@ public class Produto {
 
 	public void setCategoria(Categoria categoria) {
 		this.categoria = categoria;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
 	}
 
 }
